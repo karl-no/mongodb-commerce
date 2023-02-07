13d6411b60c8970bfd7db9e46c31cf36379e307c
karl-no <karl.publicidade@gmail.com>
2023-02-07 16:14:30 -0300

    feat: Requisito 6

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,4 +1,3 @@
-// Retorne o nome, as curtidas e vendidos dos lanches que tiveram quantidade de curtidas igual a 36 ou tenham a quantidade de vendas igual a 85
 db.produtos.find(
   {
     $or: [
---
+++ challenges/desafio6.js
@@ -0,0 +1,7 @@
+//  Retorne o nome e as curtidas dos lanches que tiveram curtidas maiores que 10 e menores que 100
+db.produtos.find(
+  {
+    curtidas: { $gt: 10, $lt: 100 },
+  },
+  { _id: 0, nome: 1, curtidas: 1 },
+);
