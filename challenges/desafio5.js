23895ad63188ca881396a088c5fd1c70b917cfc3
karl-no <karl.publicidade@gmail.com>
2023-02-07 16:00:41 -0300

    feat: Requisito 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,10 @@
+// Retorne o nome, as curtidas e vendidos dos lanches que tiveram quantidade de curtidas igual a 36 ou tenham a quantidade de vendas igual a 85
+db.produtos.find(
+  {
+    $or: [
+      { curtidas: { $eq: 36 } },
+      { vendidos: { $eq: 85 } },
+    ],
+  },
+  { _id: 0, nome: 1, curtidas: 1, vendidos: 1 },
+);
