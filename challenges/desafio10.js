5ef2a0541c4062a9f56867998e0a871082615f1b
karl-no <karl.publicidade@gmail.com>
2023-02-07 16:58:23 -0300

    feat: Requisito 11

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,3 @@
-// Retorne o nome de todos os lanches que tenham o percentual de proteínas maior ou igual a 30 e menor ou igual a 40
 db.produtos.find(
   {
     "valoresNutricionais.3.percentual": { $gte: 30, $lte: 40 },
