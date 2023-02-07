82285148007ec0260a653b2b94abaaafd069cba2
karl-no <karl.publicidade@gmail.com>
2023-02-07 17:13:30 -0300

    feat: Requisito 13

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,4 +1,3 @@
-//  Retorne o nome do produto, a quantidade de curtidas e quantos itens foram vendidos dos produtos que não sejam iguais a Big Mac e McChicken
 db.produtos.find(
   {
     nome: {
--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,9 +1,3 @@
-// Adicione ketchup aos ingredientes para todos os sanduíches menos o McChicken, garantindo que não haja duplicidade nos ingredientes
-// Para isso, escreva no arquivo desafio12.js duas queries, nesta ordem:
-
-// Crie uma query que adicione ketchup aos ingredientes para todos os sanduíches menos o McChicken, garantindo que não haja duplicidade nos ingredientes.
-
-// Crie uma query que retorne o nome e ingredientes de todos os documentos.
 db.produtos.updateMany(
   {
     nome: { $ne: "McChicken" },
