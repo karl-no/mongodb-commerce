84c683301c7a9bc9ec0784071829f04a4131a3d4
karl-no <karl.publicidade@gmail.com>
2023-02-07 18:51:47 -0300

    feat: Requisito 22

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,9 +1,3 @@
-// Remova o primeiro ingrediente do sanduíche Quarteirão com Queijo
-// Para isso, escreva no arquivo desafio20.js duas queries, nesta ordem:
-
-// Crie uma query que faça a remoção do primeiro ingrediente no sanduíche Quarteirão com Queijo.
-
-// Crie uma query que retorne o nome e ingredientes de todos os documentos.
 db.produtos.updateOne(
   { nome: "Quarteirão com Queijo" },
   {
--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,9 +1,3 @@
-// Remova o último ingrediente do sanduíche Cheddar McMelt
-// Para isso, escreva no arquivo desafio21.js duas queries, nesta ordem:
-
-// Crie uma query que faça a remoção do último ingrediente no sanduíche Cheddar McMelt.
-
-// Crie uma query que retorne o nome e ingredientes de todos os documentos.
 db.produtos.updateOne(
   { nome: "Cheddar McMelt" },
   {
