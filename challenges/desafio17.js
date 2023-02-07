86cd2f993f44996d0b39f7fad72ba08b564cf3bf
karl-no <karl.publicidade@gmail.com>
2023-02-07 18:00:53 -0300

    feat: Requisito 18

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,9 +1,3 @@
-// Retorne a quantidade total de produtos em uma nova coleção chamada resumoProdutos
-// Para isso, escreva no arquivo desafio17.js duas queries, nesta ordem:
-
-// Crie uma query que insira na coleção resumoProdutos um documento com os campos: franquia com o valor McDonalds e totalProdutos com o valor sendo a quantidade total de produtos registrados na coleção produtos.
-
-// Crie uma query que retorne os campos franquia e o totalProdutos da coleção resumoProdutos, resultantes da primeira query.
 db.resumoProdutos.insertOne(
   {
     franquia: "McDonalds",
